5ec7aff5ff0019dc280627f6b6a537f2971d6929
ItamarAmith <itamaram@gmail.com>
2018-07-15 11:12:55 +1000

    Add training file

---
+++ train_th.py
@@ -0,0 +1,21 @@
+import gym
+
+from baselines import deepq
+
+
+def main():
+    env = gym.make("th-v0")
+    model = deepq.models.cnn_to_mlp(
+        convs=[(1, 5, 1), (1, 5, 1)],
+        hiddens=[7 * 8 * 2],
+    )
+    act = deepq.learn(
+        env,
+        q_func=model
+    )
+    print("Saving model to cartpole_model.pkl")
+    act.save("th_model.pkl")
+
+
+if __name__ == '__main__':
+    main()
