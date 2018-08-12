423b97f4b85c91f72d26c2e2a53cc9e3285a6234
ItamarAmith <itamaram@gmail.com>
2018-08-12 22:43:15 +1000

    Include legal moves in observation space

--- gym_th/envs/th_env.py
+++ gym_th/envs/th_env.py
@@ -13,7 +13,7 @@ class ForgivingEnv(gym.Env):
         s.mount('http://', HTTPAdapter(max_retries=5))
         self.endpoint = 'http://th.local/api/th/'
         self.action_space = spaces.Discrete(7 * 8 * 2)
-        self.observation_space = spaces.Box(low=0, high=2, shape=(8, 8, 8), dtype=np.uint8)
+        self.observation_space = spaces.Box(low=0, high=2, shape=(10, 8, 8), dtype=np.uint8)
         self.board = None
         self.turnsLeft = None
         self.turnsPlayed = None
@@ -22,27 +22,41 @@ class ForgivingEnv(gym.Env):
     def step(self, action):
         x1, y1, x2, y2 = ForgivingEnv._parse_action(action)
         payload = {
-            'state': {'turnsPlayed': self.turnsPlayed, 'turnsLeft': self.turnsLeft, 'board': self.board.tolist()},
+            'state': {'turnsPlayed': self.turnsPlayed, 'turnsLeft': self.turnsLeft, 'board': self.board},
             'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2}
         # TODO: Error handling
         j = requests.post(self.endpoint + 'move', json=payload).json()
         self._save_game_state(j['state'])
         reward = 1 if j['isSuccess'] else 0
 
-        return self.board, reward, self.turnsLeft <= 0, {}
+        return self.obs, reward, self.turnsLeft <= 0, {}
 
     def reset(self):
         r = requests.get(self.endpoint + 'new', params={'turns': 5})
         self._save_game_state(r.json())
-        return self.board
+        return self.obs
 
     def _save_game_state(self, j):
-        self.board = np.array(j['board'], dtype=np.uint8)
+        self.board = j['board']
         self.turnsLeft = j['turnsLeft']
         self.turnsPlayed = j['turnsPlayed']
+        self.obs = np.array(ForgivingEnv._generate_obs(j['board'], j['availableMoves']), dtype=np.uint8)
 
     def render(self, mode='human', close=False):
         pass
+
+    @staticmethod
+    def _generate_obs(board, moves):
+        b = [[[1 if i == t else 0 for i in line] for line in board] for t in range(8)]
+        mhorz = [x[:] for x in [[0] * 8] * 8]
+        mvert = [x[:] for x in [[0] * 8] * 8]
+        for m in moves:
+            if m['x2'] - m['x1']:
+                mhorz[m['x1']][m['y1']] = 1
+            elif m['y2'] - m['y1']:
+                mvert[m['x1']][m['y1']] = 1
+
+        return b + [mhorz] + [mvert]
 
     @staticmethod
     def _parse_action(action):
